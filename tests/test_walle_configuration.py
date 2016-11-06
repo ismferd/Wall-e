6a80d8ccadbd916a6b22c4898e4156a90172ec0d
ismael.fernandez <ismael.fernandez@scmspain.com>
2016-11-06 13:38:56 +0100

    simple unit test

--- tests/test_walle_configuration.py
+++ tests/test_walle_configuration.py
@@ -1,6 +1,8 @@
 from src.wall_e import WalleConfiguration
+from src.clean_ec2_instances import CleanerEc2Instances
 from src.clean_launchconfiguration import CleanerLaunchConfiguration
 from src.boto_connections import BotoConnections
+from src.clean_cloudformation import CleanerCloudFormation
 import mock
 
 
@@ -8,11 +10,31 @@ class TestWalleConfiguration(object):
     def test_clean_launch_configuration_cleaner_launch_config_called(self):
         boto = mock.Mock(BotoConnections)
         walle = WalleConfiguration(boto)
-        clc = CleanerLaunchConfiguration
-        clc.get_all_launch_configuration = mock.Mock()
-        clc.cleaner_launch_config = mock.Mock()
+        lc_cleaner = CleanerLaunchConfiguration
+        lc_cleaner.get_all_launch_configuration = mock.Mock()
+        lc_cleaner.cleaner_launch_config = mock.Mock()
         walle.clean_launchconfiguration('resource', 'account')
-        assert clc.cleaner_launch_config.called
+        assert lc_cleaner.cleaner_launch_config.called
+
+    def test_clean_ec2_instances_cleaner_ec2_instances_by_tag_called(self):
+        boto = mock.Mock(BotoConnections)
+        walle = WalleConfiguration(boto)
+        ec2_instances = CleanerEc2Instances
+        ec2_instances.save_ec2_instances_by_tag = mock.Mock()
+        ec2_instances.cleaner_ec2_instances_by_tag = mock.Mock()
+        walle.clean_ec2_instances('resource', 'account', 'tag')
+        assert ec2_instances.cleaner_ec2_instances_by_tag.called
+
+    def test_clean_cloudformation_cleaner_stacks_called(self, tmpdir):
+        boto = mock.Mock(BotoConnections)
+        dust = tmpdir.mkdir('dust').join('cloudformation_dust')
+        dust.write("cloudformation_test")
+        walle = WalleConfiguration(boto)
+        cloudformation = CleanerCloudFormation
+        cloudformation.get_stacks_names = mock.Mock()
+        cloudformation.cleaner_stacks = mock.Mock()
+        walle.clean_cloudformation('resource', 'account', 'src/dust/cloudformation_dust')
+        assert cloudformation.cleaner_stacks.called
 
 
 
