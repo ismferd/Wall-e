6cf24870369e648fe4eff766222c8ce0581b7493
stealthizer <stealthizer@gmail.com>
2016-11-06 16:54:24 +0100

    added total of deleted snapshots, deleted extra debugging prints

--- src/clean_snapshots.py
+++ src/clean_snapshots.py
@@ -11,7 +11,6 @@ class CleanerSnapshots(object):
         self.connection_snapshots = connection_snapshots
 
     def get_all_snapshots(self):
-        print("lol")
         return self.connection_snapshots.describe_snapshots()
 
     def delete_snapshot(self, snapshotId):
@@ -24,19 +23,21 @@ class CleanerSnapshots(object):
             '''
             check if we have to retry due to AWS throttling, fail otherwise
             '''
-            print("muerte")
         else:
             return result
 
     def cleaner_snapshots_older_than(self, days):
+        deleted_snapshots = 0
         snapshots = self.get_all_snapshots()['Snapshots']
         logger.info('Maintaining only snapshots newer than {0} days'.format(str(days)))
         for snapshot in snapshots:
             snapshot_date = snapshot['StartTime']
             tz_info = snapshot_date.tzinfo
             if (datetime.now(tz_info)) > (snapshot_date + timedelta(days=int(days[0]))):
+                deleted_snapshots += 1
                 logger.info('Deleting snapshot {0} creation date {1}'.format(snapshot['SnapshotId'],snapshot_date))
                 '''
                 If the snapshot date is greater than the number of days specified, delete it
                 # delete_snapshot(snapshot['SnapshotId']
                 '''
+        logger.info('Total deleted snapshots:{0}'.format(deleted_snapshots))
